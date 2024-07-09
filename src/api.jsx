c7bc0ee9abbf5732139c007bbefb871d5fc6fe86
Khairat Adesina <khairatadesina01@gmail.com>
2024-07-09 13:46:39 +0100

    add: added baseURL to .env and create a prod build

--- src/api.jsx
+++ src/api.jsx
@@ -2,10 +2,7 @@
 
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? `http://localhost:3050`
-    : `http://localhost:3050`;
+const baseURL = import.meta.env.VITE_APP_BASEURL;
 
 // const baseURL = `https://crm.mansastars.com`;
 const api = axios.create({
