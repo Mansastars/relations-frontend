6594858eb1f36ed1209b5e1e005aa56b5a2938fa
rightguydaniel <rightguydaniel@gmail.com>
2024-08-12 16:29:40 +0100

    fix

---
+++ dist/assets/x-circle-DW-q6KQ6.js
@@ -0,0 +1,11 @@
+import{c as e}from"./createLucideIcon-DxUil7X2.js";/**
+ * @license lucide-react v0.320.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("Pen",[["path",{d:"M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z",key:"5qss01"}]]);/**
+ * @license lucide-react v0.320.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("XCircle",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]]);export{a as P,r as X};
