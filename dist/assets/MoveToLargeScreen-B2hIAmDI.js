d3389ea2f80048f5b9317f8f4a3f5244b5b3e509
rightguydaniel <rightguydaniel@gmail.com>
2024-09-19 11:44:14 +0100

    added broadcast mail feature

---
+++ dist/assets/MoveToLargeScreen-B2hIAmDI.js
@@ -0,0 +1,1 @@
+import{C as e}from"./index-Vc1HX7bu.js";function o(){return e.jsx("h2",{className:"text-center text-xl font-bold mt-20 bg-[#f2f3ff] text-dark-blue",children:"Please move to a larger screen to access the dashboard or flip your mobile device horizontally."})}export{o as default};
